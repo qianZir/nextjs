17170544b2210307941744851037c71c71d4ee96
qianZir <353122784@qq.com>
2022-05-18 16:16:06 +0800

    添加nextjs

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import '../styles/common.css';
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ pages/demo/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+function index() {
+  return (
+    <div>
+      123
+    </div>
+  )
+}
+
+export default index
